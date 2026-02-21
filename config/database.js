e0020958507d4d8d1605e14c2d2f4bb41ef225c4
Henrydev999 <henry4real2007@gmail.com>
2026-02-21 19:18:07 +0100

    add structure

---
+++ config/database.js
@@ -0,0 +1,31 @@
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    postgres: {
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', true)
+          ? { rejectUnauthorized: false }
+          : false,
+      },
+    },
+    sqlite: {
+      connection: {
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      },
+      useNullAsDefault: true,
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+    },
+  };
+};
