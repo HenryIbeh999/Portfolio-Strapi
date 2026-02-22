2f4f9745f0965b85cae0f4e5faac7ae50b31b1cd
Henrydev999 <henry4real2007@gmail.com>
2026-02-22 17:15:21 +0100

    update database.js

--- config/database.js
+++ config/database.js
@@ -1,31 +1,27 @@
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
-  const connections = {
-    postgres: {
+  if (client === 'postgres') {
+    return {
       connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', true)
-          ? { rejectUnauthorized: false }
-          : false,
+        client: 'postgres',
+        connection: {
+          connectionString: env('DATABASE_URL'),
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
       },
-    },
-    sqlite: {
+    };
+  }
+
+  return {
+    connection: {
+      client: 'sqlite',
       connection: {
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       useNullAsDefault: true,
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
     },
   };
 };
